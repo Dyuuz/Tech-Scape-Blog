db61c1784d7bb93b03788af767f81503abae3225
Dyuuz <lekanoyesunle@gmail.com>
2025-02-01 07:23:43 +0100

    blog updated with utils function

---
+++ App/utils/utility.py
@@ -0,0 +1,169 @@
+import json
+from django.shortcuts import render,redirect, get_object_or_404
+from django.core import serializers
+from datetime import datetime
+from django.core.cache import cache
+from django import template
+from django.urls import reverse
+from requests import request
+from django.http import JsonResponse, HttpResponse
+from App.models import *
+import random
+from itertools import chain
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib.auth.hashers import make_password, check_password
+from django.db.models import Count
+import re
+
+# Function scripts
+def subscribe_check(request):
+    current_user = request.user if request.user.is_authenticated else None
+    user_subscribed = Newsletter.objects.get(user=current_user)
+    user_subscribed = True if user_subscribed and user_subscribed.subscribe else False
+    return user_subscribed
+
+# Pending 
+def comment(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        comment = request.POST.get('comment')
+        pk = request.POST.get('pk')
+        pk = int(pk)
+        blog = Blog.objects.get(pk=pk)
+        
+        protocol = Comments(name=name, comment=comment,category=blog)
+        return HttpResponse("Comment saved!")
+    return HttpResponse("Error Loading page")
+
+# Caching operation of converting serialized data to a default data structure 
+def deserial(data):
+    # Function to deserialize data if serialized data is cached
+    images_data = serializers.deserialize('json', data)
+    
+    # Extract the model instances to a list from images_data json structure
+    images_data = [obj.object for obj in images_data] 
+    # print(f" data {images_data}")
+    return images_data
+
+# Ajax request to update user's interaction on a blog's like feature 
+def update_like(request):
+    """
+    Function to update user like feature directly with ajax request 
+    """
+    if request.method == "POST":
+        data = request.body
+        Ajax_data = json.loads(data.decode('utf-8'))
+        user = request.user
+        post_id = int(Ajax_data.get('post_id'))
+        buttonBoolean = Ajax_data.get('buttonBoolean')
+        likeBoolean = Ajax_data.get('likeBoolean')
+        # print(buttonBoolean)
+        # print(user.like_post.all())
+        # print(user.bookmark_post.all())
+        blog = Blog.objects.get(id=post_id)
+        
+        if buttonBoolean == "true":
+            blog.likes.add(user)
+            blog.save()
+            return JsonResponse({'success': True, 'message': 'Post liked successfully!'})
+        else:
+            blog.likes.remove(user)
+            blog.save()
+            return JsonResponse({'success': True, 'message': 'Post unliked successfully!'})
+
+# Ajax request to update user's interaction on a blog's bookmark feature 
+def update_bookmark(request):
+    """
+    Logic script to update user bookmark feature directly with ajax request 
+    """
+    if request.method == "POST":
+        data = request.body
+        Ajax_data = json.loads(data.decode('utf-8'))
+        user = request.user
+        post_id = int(Ajax_data.get('post_id'))
+        buttonBoolean = Ajax_data.get('buttonBoolean')
+        blog = Blog.objects.get(id=post_id)
+        
+        if buttonBoolean == "true":
+            blog.bookmarks.add(user)
+            blog.save()
+            return JsonResponse({'success': True, 'message': 'Post bookmarked successfully!'})
+        else:
+            blog.bookmarks.remove(user)
+            blog.save()
+            return JsonResponse({'success': True, 'message': 'Post unbookmarked successfully!'})
+
+def get_blogs_based_on_user_likes(user):
+    # Step 1: Get the user's top 3 most liked categories
+    top_categories = (
+        Category.objects.filter(posts__likes=user)  # Filter categories where user liked blogs
+        .annotate(like_count=Count('posts__likes'))  # Count likes in each category
+        .order_by('-like_count')  # Sort by most liked categories
+        .values_list('id', flat=True)[:4]  # Get IDs of the top 4 categories
+    )
+
+    # Step 2: Fetch blog posts, picking 3 posts from each top category
+    blogs = []
+    for category_id in top_categories:
+        category_blogs = Blog.objects.filter(category_id=category_id).order_by('-time')[:3]  # Get latest 3 posts
+        blogs.append(category_blogs)
+
+    # Flatten the list of QuerySets into a single list
+    final_blog_list = list(chain(*blogs))
+
+    # Step 3: If less than 11 posts, fill remaining spots with random blog posts from other categories
+    if len(final_blog_list) < 11:
+        remaining_count = 11 - len(final_blog_list)
+        
+        # Get random blog posts not in the user's top categories
+        additional_blogs = Blog.objects.exclude(category_id__in=top_categories).order_by('?')[:remaining_count]
+        
+        # Combine both lists
+        final_blog_list.extend(additional_blogs)
+
+    return final_blog_list[:11]  # Ensure it returns exactly 11 posts
+
+def update_subscribe(request):
+    if request.method == "POST":
+        data = request.body
+        Ajax_data = json.loads(data.decode('utf-8'))
+        emailVal = Ajax_data.get('emailVal')
+        email_regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+        if not re.match(email_regex, emailVal):
+            return JsonResponse({'success': False, 'message': 'Invalid email format!'})
+        else:
+            try:
+                user = request.user
+                if user:
+                    subscribe = Newsletter.objects.get(user=user)
+                    subscribe.subscribe = True
+                    subscribe.email = emailVal
+                    subscribe.save()
+                    return JsonResponse({'success': True, 'message': 'Subscribed successfully!'})
+                else:
+                    return JsonResponse({'success': False, 'message': 'Sign in to subscribe!'})
+            except:
+                return JsonResponse({'success': False, 'message': 'Sign in to subscribe!'})
+            
+def update_shares(request):
+    """
+    Logic script to update blog shares count feature directly with ajax request 
+    """
+    if request.method == "POST":
+        data = request.body
+        Ajax_data = json.loads(data.decode('utf-8'))
+        post_id = int(Ajax_data.get('post_id'))
+        blog = Blog.objects.get(id=post_id)
+        user = request.user
+        
+        session_share_key = f'viewed_post_{post_id}_{user}'
+        if not request.session.get(session_share_key):
+            request.session[session_share_key] = True
+            if blog:
+                blog.shares_count += 1
+                blog.save()
+                return JsonResponse({'success': True, 'message': 'Blog count updated successfully!'})
+            
+        else:
+            return JsonResponse({'success': False, 'message': 'Too many requests!'})
--- App/views.py
+++ App/views.py
@@ -9,6 +9,7 @@ from django.core.cache import cache
 from django import template
 from django.urls import reverse
 from .models import *
+from App.utils.utility import *
 import random
 from itertools import chain
 from django.contrib.auth import authenticate, login, logout
@@ -26,8 +27,7 @@ def home(request):
         all_user_likes = current_user.like_post.all()
         all_user_likes = [int(blog.id) for blog in all_user_likes]
         
-        user_subscribed = Newsletter.objects.get(user=current_user)
-        user_subscribed = True if user_subscribed and user_subscribed.subscribe else False
+        user_subscribed = subscribe_check(request)
         
         all_user_bookmarks = current_user.bookmark_post.all()
         
@@ -118,6 +118,8 @@ def recentposts(request):
     recentpost = Blog.objects.all().order_by('-time')
     
     cache_key = f"category_images_recentposts"
+    
+    user_subscribed = subscribe_check(request)
     
     # Try to get the list of images from cache
     images_data_recent = cache.get(cache_key)
@@ -265,6 +267,9 @@ def categories(request):
                 time_diff = hours_difference - 60
                 mod.time = f"{time_diff:.0f}"
                 mod.time = int(mod.time)
+                
+    user_subscribed = subscribe_check(request)
+    
     return render(request, 'categories.html', locals())
 
 # Displays all blog posts in a category 
@@ -296,6 +301,8 @@ def category_detail(request, name):
     
     show_ellipsis = total_pages - current_page 
     
+    user_subscribed = subscribe_check(request)
+    
     return render(request, 'category_detail.html', locals())
 
 # Displays a blog post full content
@@ -324,158 +331,17 @@ def postpage(request, name, slug):
         is_liked = blog in user.like_post.all()
         is_bookmarked = blog in user.bookmark_post.all()
         # all = user.like_post.all()
+        
+    user_subscribed = subscribe_check(request)
     
     return render(request, 'postpage.html', locals())
 
-# Pending 
-def comment(request):
-    if request.method == "POST":
-        name = request.POST.get('name')
-        comment = request.POST.get('comment')
-        pk = request.POST.get('pk')
-        pk = int(pk)
-        blog = Blog.objects.get(pk=pk)
-        
-        protocol = Comments(name=name, comment=comment,category=blog)
-        return HttpResponse("Comment saved!")
-    return HttpResponse("Error Loading page")
-
-# Caching operation of converting serialized data to a default data structure 
-def deserial(data):
-    # Function to deserialize data if serialized data is cached
-    images_data = serializers.deserialize('json', data)
-    
-    # Extract the model instances to a list from images_data json structure
-    images_data = [obj.object for obj in images_data] 
-    # print(f" data {images_data}")
-    return images_data
-
-# Ajax request to update user's interaction on a blog's like feature 
-def update_like(request):
-    """
-    Function to update user like feature directly with ajax request 
-    """
-    if request.method == "POST":
-        data = request.body
-        Ajax_data = json.loads(data.decode('utf-8'))
-        user = request.user
-        post_id = int(Ajax_data.get('post_id'))
-        buttonBoolean = Ajax_data.get('buttonBoolean')
-        likeBoolean = Ajax_data.get('likeBoolean')
-        # print(buttonBoolean)
-        # print(user.like_post.all())
-        # print(user.bookmark_post.all())
-        blog = Blog.objects.get(id=post_id)
-        
-        if buttonBoolean == "true":
-            blog.likes.add(user)
-            blog.save()
-            return JsonResponse({'success': True, 'message': 'Post liked successfully!'})
-        else:
-            blog.likes.remove(user)
-            blog.save()
-            return JsonResponse({'success': True, 'message': 'Post unliked successfully!'})
-
-# Ajax request to update user's interaction on a blog's bookmark feature 
-def update_bookmark(request):
-    """
-    Logic script to update user bookmark feature directly with ajax request 
-    """
-    if request.method == "POST":
-        data = request.body
-        Ajax_data = json.loads(data.decode('utf-8'))
-        user = request.user
-        post_id = int(Ajax_data.get('post_id'))
-        buttonBoolean = Ajax_data.get('buttonBoolean')
-        blog = Blog.objects.get(id=post_id)
-        
-        if buttonBoolean == "true":
-            blog.bookmarks.add(user)
-            blog.save()
-            return JsonResponse({'success': True, 'message': 'Post bookmarked successfully!'})
-        else:
-            blog.bookmarks.remove(user)
-            blog.save()
-            return JsonResponse({'success': True, 'message': 'Post unbookmarked successfully!'})
-
 def profile(request):
+    user_subscribed = subscribe_check(request)
     try:
         user = request.user
         all_user_likes = user.like_post.all()
+        
         return render(request , 'profile.html', locals())
     except:
         return redirect('home')
-
-def get_blogs_based_on_user_likes(user):
-    # Step 1: Get the user's top 3 most liked categories
-    top_categories = (
-        Category.objects.filter(posts__likes=user)  # Filter categories where user liked blogs
-        .annotate(like_count=Count('posts__likes'))  # Count likes in each category
-        .order_by('-like_count')  # Sort by most liked categories
-        .values_list('id', flat=True)[:4]  # Get IDs of the top 4 categories
-    )
-
-    # Step 2: Fetch blog posts, picking 3 posts from each top category
-    blogs = []
-    for category_id in top_categories:
-        category_blogs = Blog.objects.filter(category_id=category_id).order_by('-time')[:3]  # Get latest 3 posts
-        blogs.append(category_blogs)
-
-    # Flatten the list of QuerySets into a single list
-    final_blog_list = list(chain(*blogs))
-
-    # Step 3: If less than 11 posts, fill remaining spots with random blog posts from other categories
-    if len(final_blog_list) < 11:
-        remaining_count = 11 - len(final_blog_list)
-        
-        # Get random blog posts not in the user's top categories
-        additional_blogs = Blog.objects.exclude(category_id__in=top_categories).order_by('?')[:remaining_count]
-        
-        # Combine both lists
-        final_blog_list.extend(additional_blogs)
-
-    return final_blog_list[:11]  # Ensure it returns exactly 11 posts
-
-def update_subscribe(request):
-    if request.method == "POST":
-        data = request.body
-        Ajax_data = json.loads(data.decode('utf-8'))
-        emailVal = Ajax_data.get('emailVal')
-        email_regex = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-        if not re.match(email_regex, emailVal):
-            return JsonResponse({'success': False, 'message': 'Invalid email format!'})
-        else:
-            try:
-                user = request.user
-                if user:
-                    subscribe = Newsletter.objects.get(user=user)
-                    subscribe.subscribe = True
-                    subscribe.email = emailVal
-                    subscribe.save()
-                    return JsonResponse({'success': True, 'message': 'Subscribed successfully!'})
-                else:
-                    return JsonResponse({'success': False, 'message': 'Sign in to subscribe!'})
-            except:
-                return JsonResponse({'success': False, 'message': 'Sign in to subscribe!'})
-            
-def update_shares(request):
-    """
-    Logic script to update blog shares count feature directly with ajax request 
-    """
-    if request.method == "POST":
-        data = request.body
-        Ajax_data = json.loads(data.decode('utf-8'))
-        post_id = int(Ajax_data.get('post_id'))
-        blog = Blog.objects.get(id=post_id)
-        user = request.user
-        
-        session_share_key = f'viewed_post_{post_id}_{user}'
-        if not request.session.get(session_share_key):
-            request.session[session_share_key] = True
-            if blog:
-                blog.shares_count += 1
-                blog.save()
-                return JsonResponse({'success': True, 'message': 'Blog count updated successfully!'})
-            
-        else:
-            return JsonResponse({'success': False, 'message': 'Too many requests!'})
