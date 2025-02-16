81d9fa8ade672c0f8a14d90980ecb21221503482
Dyuuz <lekanoyesunle@gmail.com>
2025-02-16 18:44:28 +0100

    AbstractUser

--- App/models.py
+++ App/models.py
@@ -3,8 +3,17 @@ from django.utils import timezone
 import uuid
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+from Blog import settings
 
 # Create your models here.
+class Client(AbstractUser):
+    account_verified = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.username
+
+
 class Category(models.Model):
     name = models.CharField(max_length=100)
     bio = models.CharField(max_length=100, default='Welcome')
@@ -16,7 +25,7 @@ class Category(models.Model):
 
 class Newsletter(models.Model):
     subscribe = models.BooleanField(default=False)
-    user = models.ForeignKey(User, related_name='subscribe_list', on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='subscribe_list', on_delete=models.CASCADE)
     email = models.EmailField(max_length=100, default='empty')
     
     def __str__(self) -> None:
@@ -52,7 +61,7 @@ class Comments(models.Model):
         return self.name
     
 class PasswordReset(models.Model):
-    user = models.ForeignKey(User, related_name='token_list', on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='token_list', on_delete=models.CASCADE)
     token = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
     time = models.DateTimeField(auto_now_add=True)
     
