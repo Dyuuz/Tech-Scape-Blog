1c2aa24408321ca41b6bc37f0ffb4bb28a9276bd
Dyuuz <lekanoyesunle@gmail.com>
2025-02-21 14:28:33 +0100

    Profile Request

--- App/static/JavaScript/profile67.js
+++ App/static/JavaScript/profile67.js
@@ -21,14 +21,6 @@ document.addEventListener("DOMContentLoaded", function () {
       editProfileModal.style.display = "none";
     });
   
-    // editProfileForm.addEventListener("submit", (e) => {
-    //   e.preventDefault();
-    //   profileName.textContent = document.getElementById("name").value;
-    //   profileEmail.textContent = document.getElementById("email").value;
-    //   profileBio.textContent = document.getElementById("bio").value;
-    //   editProfileModal.style.display = "none";
-    // });
-  
     document.getElementById('editProfileForm').onsubmit = function(event) {
       var username = document.querySelector('input[name="username"]').value;
       var email = document.querySelector('input[name="email"]').value;
@@ -41,39 +33,39 @@ document.addEventListener("DOMContentLoaded", function () {
         alert("hola");
       }
 
-      // axios.post('update-profile', formData, {
-      //   headers : {
-      //     'X-CSRFToken': csrf_token,
-      //   }
-      // }).then((response) => {
-      //   if (response.data.success === true) {
-      //     Swal.fire({
-      //       title: response.data.message,
-      //       text: 'You can now log in with your new password',
-      //       icon: 'success',
-      //       showConfirmButton: true,
-      //       confirmButtonText: 'Continue',
-      //       confirmButtonColor: 'rgb(58, 138, 222)', 
-      //       allowOutsideClick: false,
-      //       allowEscapeKey: false,
-      //       allowEnterKey: false
-      //     }).then((result) => {
-      //         if (result.isConfirmed) {
-      //             window.location.reload();
-      //         }
-      //     });
-      //   } else {
-      //       alertmsg.textContent = response.data.message;
-      //   }
-      // }).catch((error) => {
-      //   let errorMessage = 'Mail service down. Fix in progress.';
-      //   if (error.response) {
-      //       errorMessage = `Request error, please refresh and try again.`;
-      //   } else if (error.request) {
-      //       errorMessage = 'Please check your network connection.';
-      //   }
-      //   alertmsg.textContent = errorMessage;
-      // });
+      axios.post('update-profile', formData, {
+        headers : {
+          'X-CSRFToken': csrf_token,
+        }
+      }).then((response) => {
+        if (response.data.success === true) {
+          Swal.fire({
+            title: response.data.message,
+            text: 'You can now log in with your new password',
+            icon: 'success',
+            showConfirmButton: true,
+            confirmButtonText: 'Continue',
+            confirmButtonColor: 'rgb(58, 138, 222)', 
+            allowOutsideClick: false,
+            allowEscapeKey: false,
+            allowEnterKey: false
+          }).then((result) => {
+              if (result.isConfirmed) {
+                  window.location.reload();
+              }
+          });
+        } else {
+            alertmsg.textContent = response.data.message;
+        }
+      }).catch((error) => {
+        let errorMessage = 'Mail service down. Fix in progress.';
+        if (error.response) {
+            errorMessage = `Request error, please refresh and try again.`;
+        } else if (error.request) {
+            errorMessage = 'Please check your network connection.';
+        }
+        alertmsg.textContent = errorMessage;
+      });
       event.preventDefault(); 
     };
 
