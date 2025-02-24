b1dd5c82c64c2c54ac9460f59b5419f2c10ec82c
Lekan Oyesunle <lekanoyesunle@gmail.com>
2025-02-24 19:42:16 +0100

    Update settings.py

--- Blog/settings.py
+++ Blog/settings.py
@@ -94,17 +94,13 @@ WSGI_APPLICATION = 'Blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-
- DATABASES = {
-     'default': {
-         'ENGINE': env('PGS_ENGINE', default=os.getenv('PGS_ENGINE')) ,
-         'NAME': env('PGS_NAME', default=os.getenv('PGS_NAME')),      
-         'USER': env('PGS_USER', default=os.getenv('PGS_USER')),            
-         'PASSWORD':env('PGS_PASSWORD', default=os.getenv('PGS_PASSWORD')) ,   
-         'HOST': env('PGS_HOST', default=os.getenv('PGS_HOST')),    
-         'PORT': env.int('PGS_PORT', default=os.getenv('PGS_PORT')),
-     }
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
+ 
 
 JAZZMIN_SETTINGS = {
     "site_title": "Tech Scape",
