a908e4d2ed19d0bb5f97d0d6be9fa9b2127f4643
Dyuuz <lekanoyesunle@gmail.com>
2025-03-12 17:44:31 +0100

    update app featured endpoint

--- staticfiles/JavaScript/Featured.js
+++ staticfiles/JavaScript/Featured.js
@@ -1,8 +1,15 @@
 function premium() {
-    Swal.fire({
-        title: 'Premium feature',
-        text: 'We are currently working on this feature. Please check back later.',
-        icon: 'info',
-        showConfirmButton: true,
-    });
+    premiumplus = document.querySelector('.premium');
+    text = premiumplus.getAttribute('data-text');
+    text.textContent = 'Premium+';
+    // Swal.fire({
+    //     title: 'Premium feature',
+    //     text: 'We are currently working on this feature. Please check back later.',
+    //     icon: 'info',
+    //     showConfirmButton: true,
+    //     confirmButtonColor: 'rgb(58, 138, 222)',
+    //     allowOutsideClick: false,
+    //     allowEscapeKey: false,
+    //     allowEnterKey: false
+    // });
 };
