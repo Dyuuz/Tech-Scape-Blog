b27c70e0d32d1e53d99ec2f680da16ab831b3add
Dyuuz <lekanoyesunle@gmail.com>
2025-03-12 17:21:19 +0100

    featured product update

---
+++ App/static/JavaScript/Featured.js
@@ -0,0 +1,8 @@
+function premium() {
+    Swal.fire({
+        title: 'Premium feature',
+        text: 'We are currently working on this feature. Please check back later.',
+        icon: 'info',
+        showConfirmButton: true,
+    });
+};
