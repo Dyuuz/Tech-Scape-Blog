8dee09434062dd24aea6e58bfadd6904f1eed510
Dyuuz <lekanoyesunle@gmail.com>
2025-03-12 19:46:16 +0100

    update app featured endpoint

--- staticfiles/JavaScript/Featured.js
+++ staticfiles/JavaScript/Featured.js
@@ -1,16 +1,15 @@
 function premium() {
-    let premiumplus = document.querySelector('.premium');
-    premiumplus.textContent = 'Premium+';
+    // let premiumplus = document.querySelector('.premium');
+    // premiumplus.textContent = 'Premium+';
     
-    // Uncomment if you want to show a SweetAlert notification
-    // Swal.fire({
-    //     title: 'Premium feature',
-    //     text: 'We are currently working on this feature. Please check back later.',
-    //     icon: 'info',
-    //     showConfirmButton: true,
-    //     confirmButtonColor: 'rgb(58, 138, 222)',
-    //     allowOutsideClick: false,
-    //     allowEscapeKey: false,
-    //     allowEnterKey: false
-    // });
+    Swal.fire({
+        title: 'Premium feature',
+        text: 'We are currently working on this feature. Please check back later.',
+        icon: 'info',
+        showConfirmButton: true,
+        confirmButtonColor: 'rgb(58, 138, 222)',
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+        allowEnterKey: false,
+    });
 }
