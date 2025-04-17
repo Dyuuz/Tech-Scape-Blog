7bfec0c5d675290433c637047a86ee02419b7a1a
Dyuuz <lekanoyesunle@gmail.com>
2025-04-17 23:29:56 +0100

    update db

--- App/models.py
+++ App/models.py
@@ -20,9 +20,9 @@ class Client(AbstractUser):
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
-    bio = models.TextField(max_length=500, default='Welcome')
+    bio = models.TextField(max_length=500, default='Welcome', blank=True, null=True)
     image = CloudinaryField('image')
-    dp = CloudinaryField('image', default="project-management_gqhk75")
+    dp = CloudinaryField('image', default="project-management_gqhk75", blank=True, null=True)
     # slug = models.SlugField()
 
     def __str__(self) -> None:
@@ -32,18 +32,18 @@ class Newsletter(models.Model):
     subscribe = models.BooleanField(default=False)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='subscribe_list', on_delete=models.CASCADE)
     email = models.EmailField(max_length=100, default='empty')
-    
+
     def __str__(self) -> None:
         return self.user.username
-    
+
 class Blog(models.Model):
-    title = models.CharField(max_length=150)
-    username = models.CharField(max_length=20, default='anon', null=True)
+    title = models.CharField(max_length=150, blank=True, null=True)
+    username = models.CharField(max_length=20, default='anon', blank=True, null=True)
     body = CKEditor5Field("Body", config_name="default")
     slug = models.SlugField(max_length=255,unique=True,blank=True)
     time = models.DateTimeField(auto_now_add=True)
     image = CloudinaryField('image')
-    dp = CloudinaryField('image')
+    dp = CloudinaryField('image', blank=True, null=True)
     likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_post', blank=True)
     bookmarks = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='bookmark_post', blank=True)
     dislikes = models.IntegerField(default=0)
@@ -51,7 +51,7 @@ class Blog(models.Model):
     shares_count = models.IntegerField(default=0)
     mins_read = models.IntegerField(default=5)
     category = models.ForeignKey(Category, related_name='posts', on_delete=models.CASCADE, default=2)
-    
+
     def __str__(self):
         return self.title
 
@@ -64,19 +64,19 @@ class Comments(models.Model):
 
     def __str__(self) -> None:
         return self.name
-    
+
 class PasswordReset(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='token_list', on_delete=models.CASCADE)
     token = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
     time = models.DateTimeField(auto_now_add=True)
-    
+
     def is_valid(self):
         # Token expires after 15 minutes
         return self.time + timedelta(minutes=15) > timezone.now()
-    
+
     def __str__(self):
         return f"{self.user.username} - {self.token}"
-    
+
 class VerifyUser(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     token = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
@@ -84,7 +84,7 @@ class VerifyUser(models.Model):
 
     def __str__(self):
         return str(self.token)
-    
+
     def activate_verification(self):
         self.user.is_verified = True
         self.user.save()
