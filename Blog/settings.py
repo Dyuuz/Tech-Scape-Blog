2f5939721f4271148df03b1b8f0fd18624933977
Dyuuz <lekanoyesunle@gmail.com>
2025-08-08 16:20:32 +0100

    feat: updated dbg

--- Blog/settings.py
+++ Blog/settings.py
@@ -30,7 +30,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = env('SECRET_KEY', default=os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'techscape.up.railway.app',
